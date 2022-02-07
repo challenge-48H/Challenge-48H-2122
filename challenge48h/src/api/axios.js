d82d360c1a2c1bcb66025b87739b9b07b63549e7
kevinguyodo <kevin.guyodo@ynov.com>
2022-02-07 13:31:03 +0100

    get all element of API

--- challenge48h/src/api/axios.js
+++ challenge48h/src/api/axios.js
@@ -1,6 +1,9 @@
-import api from 'axios'
+import axios from "axios";
+import api from "axios";
 
-export default{ 
-
-    
-}
+export default {
+  async getMovieName() {
+    let test = axios.get("https://swapi.dev/api/films/1");
+    console.log(test);
+  },
+};
--- challenge48h/src/components/Acceuil.vue
+++ challenge48h/src/components/Acceuil.vue
@@ -1,13 +1,11 @@
 <script setup>
-
-import {useRouter} from"vue-router";
-
+import { useRouter } from "vue-router";
+import API from "../api/axios.js";
+API.getMovieName().data;
 </script>
 
 <template>
-    <h1>salut</h1>
-</template> 
+  <h1>salut</h1>
+</template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
