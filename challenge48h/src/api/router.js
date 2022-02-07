a58106c99ed5c17c534b9769cd06c6ea8fb367a3
RIVALANT NICOLAS <nicolas.rivalant@ynov.com>
2022-02-07 16:15:13 +0100

    second commit

--- challenge48h/src/api/router.js
+++ challenge48h/src/api/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router"
-import Acceuil from "../components/Acceuil.vue"
+import quiz from "../components/quiz.vue"
 
 
 export default createRouter({
---
+++ challenge48h/src/components/quiz.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+import {useRouter} from"vue-router";
+import question from "../questionnaire.js" ;
+question.question(3)
+language = "Javascript" src = "../questionnaire.js"
+</script>
+
+<template>
+    <div class = "centre">
+        <div class = "titre_centre" id="question"></div>
+    </div>
+    <div class = "colonne" id = "liste ">
+        <div class = "calque_rep" id = "rep1"></div>
+        <div class = "calque_rep" id = "rep2"></div>
+        <div class = "calque_rep" id = "rep3"></div>
+        <div class = "calque_rep" id = "rep4"></div>
+
+    </div>
+</template> 
+
+<style scoped>
+    .quiz {
+        border-width: 100px;
+        border-style: solid;
+        border-color: black;
+        border : 1rem solid;    
+    }
+</style>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
