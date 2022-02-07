32787dcc42a72a3bf5f13c479beafa96892a40e2
valentin-49 <valbouet@outlook.fr>
2022-02-07 16:37:24 +0100

    front header

--- challenge48h/src/components/Acceuil.vue
+++ challenge48h/src/components/Acceuil.vue
@@ -7,6 +7,8 @@ const name = ref ('');
 
 name.value = API.getMovieName().data;
 
+const router = useRouter();
+
 function redirect() {
     router.push("/Planets");
 }
@@ -17,7 +19,7 @@ function redirect() {
     <article class="header">
         <button class="wikiRedirect" v-on:click="redirect ()">Planet</button>
         <button class="wikiRedirect">Film</button>
-        <button class="wikiRedirect">Perssonnage</button>
+        <button class="wikiRedirect">Personnage</button>
         <button class="wikiRedirect">Vaisseau</button>
         <button class="wikiRedirect">Véhicule</button>
         <button class="wikiRedirect">Especes</button>
@@ -32,12 +34,13 @@ function redirect() {
     justify-content: space-around;
     background-color: red;
     margin-top: 0;
+    width: 100%
 }
 .wikiRedirect{
     font-family: Arial, Helvetica, sans-serif;
     font-size: 120%;
     font-weight: 600;
-    width: fit-content;
+    width: 150px;
     border-radius: 18px;
     margin: 40px auto;
     padding: 25px ;
@@ -46,5 +49,16 @@ function redirect() {
 .wikiRedirect:hover{
     padding-left: 35px;
     padding-right: 35px;
+}
+@media screen and (min-width: 200px) and (max-width: 1000px) {
+  .header {
+    display: flex;
+    flex-direction: column;
+    width: fit-content;
+  }
+  .wikiRedirect{
+    margin-left: 20px;
+    margin-right: 20px;
+  }
 }
 </style>
---
+++ challenge48h/src/components/Planet.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+    <p>jure la vie de oim j'en ai marre</p>
+</template>
+
+<style>
+
+</style>
