1055c5b0e5a1bccd33300cce7cc7b4f22862c8ce
matthieulhoo <99171104+matthieulhoo@users.noreply.github.com>
2022-02-09 00:41:45 +0100

    commit change/pull

--- challenge48h/deploy.php
+++ challenge48h/deploy.php
@@ -1,8 +1,9 @@
 <?php
 
-  //$data = print_r($_SERVER, true).json_decode(file_get_contents('php://input'));
-  //file_put_contents('./logs/'.time().'.txt', $data);
+  $data = print_r($_SERVER, true).json_decode(file_get_contents('php://input'));
+  file_put_contents('./logs/'.time().'.txt', $data);
   passthru('date');
+  'sudo git pull'
 
 ?>
 
