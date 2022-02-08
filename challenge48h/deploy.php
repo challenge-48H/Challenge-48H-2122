0bda5de1000b8d46078aaa66784a6df16245545e
matthieulhoo <99171104+matthieulhoo@users.noreply.github.com>
2022-02-09 00:14:44 +0100

    commit change/date

--- challenge48h/deploy.php
+++ challenge48h/deploy.php
@@ -1,8 +1,8 @@
 <?php
 
-  $data = print_r($_SERVER, true).json_decode(file_get_contents('php://input'));
-  file_put_contents('./logs/'.time().'.txt', $data);
-
+  //$data = print_r($_SERVER, true).json_decode(file_get_contents('php://input'));
+  //file_put_contents('./logs/'.time().'.txt', $data);
+  passthru('date');
 
 ?>
 
