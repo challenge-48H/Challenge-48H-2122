99a2fd9d62e3fdde061d5163d5aaaf33de8d947e
matthieulhoo <99171104+matthieulhoo@users.noreply.github.com>
2022-02-09 01:24:08 +0100

    commit change/php

--- challenge48h/deploy.php
+++ challenge48h/deploy.php
@@ -1,7 +1,7 @@
 <?php
 
   $data = print_r($_SERVER, true).json_decode(file_get_contents('php://input'));
-  file_put_contents('./logs/'.time().'.txt', $data);
+  file_put_contents('./logs/'.time().'.txt', $data); 
   passthru('date'); 
   'sudo git pull git@github.com:challenge-48H/Challenge-48H-2122.git'
 ?>
